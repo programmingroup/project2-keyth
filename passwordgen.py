d877b5bd8221334c0e6230a35d8c722313647529
programmingroup <luisgx14@gmail.com>
2024-11-09 11:03:54 -0500

    feat: :sparkles: Configuration modification for flask

--- passwordgen.py
+++ passwordgen.py
@@ -45,8 +45,9 @@ def index():
         </form>
         """
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
 
 
 
